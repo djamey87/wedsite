79707334d0e0fdd04086cc2c6524a96f745d2605
David Amey <djamey87@gmail.com>
2025-01-13 21:09:00 +0000

    :memo: copy changes

--- src/app/area-details/page.tsx
+++ src/app/area-details/page.tsx
@@ -5,7 +5,73 @@ export default function Index() {
     <main>
       <Container>
         <div className="text-center">
-          <p>Content coming soon!</p>
+          <h1 className="font-beyond mx-20 text-dm-grey">Things to visit</h1>
+
+          <p>
+            Things to visit around Olbia and heading towards Arbatax (wedding
+            location) and Lanusei (my hometown which is on the way to Arbatax
+            from Olbia)
+          </p>
+          <p>Picturesque villages near Olbia and Lanusei:</p>
+            
+            <ul>
+              <li>
+                San Pantaleo, picturesque village, with artisanal shops and
+                restaurants, Is very closed to the glamorous area of Porto
+                Rotondo very fancy (is the Nizza of Sardinia)
+              </li>
+              <li>
+                Posada, beautiful medieval village, with the tower/castle to
+                visit and lots of narrow streets to get lost into. Restaurants
+                and sea view, you can reach the close location of Santa Lucia
+                marina for a nice beach walk.
+              </li>
+              <li>
+                Lanusei
+              </li>
+                  <ul>
+                    <li>Visit the Bosco Selene (free parking), which includes
+                      kids park in the woods, restaurant and pizzeria in the woods(
+                      https://www.hotelboscoselene.com/index.html) and an
+                      archeological area (Nuragic civilization park) in the woods.
+                      Easy to get there coming from Olbia
+                      https://www.sardegnaturismo.it/en/explore/archaeological-park-seleni-wood.
+                      Booking for the archeological park, you can also show up there
+                      but I recommend calling first to request the guide in
+                      English.http://www.tombedeigiganti.it/visitare-il-parco-visita-il-parco/</li>
+                      <li>
+                        In Lanusei Village (free parking at Parcheggio di Piazza Mameli
+                        multistore parking, below the surface floor) take a walk in via
+                        Roma, if you are stopping there for a meal in the village I
+                        recommend . Ristorante Pizzeria Ka.Mi.Ste. alternatively La
+                        Nuova Verace ristorante Pizzeria, for an amazing gelato stop at
+                        Bar gelateria del Corso or for a drink you can visit also Nuovo
+                        Emporio Caffè. Before leaving, if you have time visit the view
+                        point called Santuario Madonna D’ Ogliastra for a breath taking
+                        view of the coastline (free parking there).
+                      </li>
+                  </ul>
+              <li></li>
+            </ul>
+            • Natural beauty near Olbia: • Pittulongu beach • Porto Istana beach
+            • Spiaggia delle Vacche beach (stunning) • Lu Impostu beach • Cala
+            brandinchi beach (stunning) • Natural reserve Isola Tavolara, I
+            recommend a boat tour:
+            https://www.getyourguide.com/tavolara-island-l142425/?date_from=2025-04-16&date_to=2025-04-16
+            Archeology Lanusei Parco Bosco Selene Things to do once arrived in
+            Arbatax: Visit the Red Rocks
+            (https://www.sardegnaturismo.it/en/explore/rocce-rosse) and walk in
+            the marina , you can have a nice gelato at the star 1 bar or a Pizza
+            at the star 2 restaurant. The area is also rich in restaurants and a
+            pharmacy. Visit the San Gemiliano Tower, you can climb it for free
+            and admire the view. Very easy to walk there from the parking space
+            and easy to climb. Is right above the Wedding venue😊, spoiler you
+            will see the venue from the top. Not suited for kids below 4 Y as
+            the ladder is a an "exposed ladder".
+            https://virtualarchaeology.sardegnacultura.it/index.php/en/archaeological-sites/eta-medievale/torri-costiere-di-arbatax/detailed-sheets/3222-la-torre-di-san-gemiliano
+            Beach in Arbatax Area CEA Beach Orri beach Foxi e Lioni Beach, sandy
+            and amazing 😊, free parking.
+          </p>
         </div>
       </Container>
     </main>
